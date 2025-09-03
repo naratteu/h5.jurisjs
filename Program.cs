b677354cd2d4523fdbf187218cfb39c93d1bc74d
박성원 <skfhddlg@naver.com>
2025-09-03 19:39:47 +0900

    h5.jurisjs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using H5;
+
+[GlobalMethods]
+public static class JurisJS
+{
+    public class Juris
+    {
+        public extern Juris(object o);
+        public extern void render(string n);
+    }
+}
